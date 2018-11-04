f66b9d3c81bffd7deb514e83103fd95caf76c2e6
Mark Anthony <markanthonymartinez9@gmail.com>
2018-11-04 10:56:44 -0600

    Feat: add NeighborhoodMap component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import NavMenu from './Components/nav-menu'
+import NavMenu from './Components/nav-menu';
+import NeighborhoodMap from './Components/NeighborhoodMap'
 import './App.css';
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Route path='/navigation' component={NavMenu}/>
+        <Route path='/map' component={NeighborhoodMap}/>
       </div>
     );
   }
