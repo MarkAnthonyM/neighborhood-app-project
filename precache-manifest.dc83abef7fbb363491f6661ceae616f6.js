b9087d6d0acf62de5c773019df6db5559250686f
Mark Anthony <markanthonymartinez9@gmail.com>
2018-11-05 23:31:04 -0600

    Updates

---
+++ precache-manifest.dc83abef7fbb363491f6661ceae616f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e59cddcac2a2f7c7ff69",
+    "url": "/MarkAnthonyM/neighborhood-app-project/static/js/runtime~main.e59cddca.js"
+  },
+  {
+    "revision": "afbdb6d8526c68036d53",
+    "url": "/MarkAnthonyM/neighborhood-app-project/static/js/main.afbdb6d8.chunk.js"
+  },
+  {
+    "revision": "2f49bc355286e934353c",
+    "url": "/MarkAnthonyM/neighborhood-app-project/static/js/1.2f49bc35.chunk.js"
+  },
+  {
+    "revision": "afbdb6d8526c68036d53",
+    "url": "/MarkAnthonyM/neighborhood-app-project/static/css/main.23422481.chunk.css"
+  },
+  {
+    "revision": "6eb3de5debbe99eb5f7c118693a91459",
+    "url": "/MarkAnthonyM/neighborhood-app-project/index.html"
+  }
+];
